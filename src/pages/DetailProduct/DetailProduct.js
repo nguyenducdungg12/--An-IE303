79b5cd41b7035781b258e215c0475ad6f4706e77
khoadangatd <66126254+khoadangatd@users.noreply.github.com>
2021-06-09 00:06:50 +0700

    'modalpic,pagination'

---
+++ src/pages/DetailProduct/DetailProduct.js
@@ -0,0 +1,129 @@
+import React,{useEffect,useState} from 'react'
+import Breadcum from '../../components/Breadcum/Breadcum'
+import CallApi from '../../helper/axiosClient'
+import "./DetailProduct.css"
+import DetailProductComponent from '../../components/DetailProduct/DetailProductComponent'
+import SlideProduct from '../../components/Slider/slideproduct';
+import ConfigProduct from '../../components/DetailProduct/ConfigProduct'
+import Comment from '../../components/DetailProduct/Comment'
+import parse from 'html-react-parser';
+
+function DetailProduct(props) {
+    const {id} = props.match.params;
+    const [MoreContent, setMoreContent] = useState(false);
+    const [Product, setProduct] = useState(null);
+   
+    useEffect(() => {
+        async function getProduct(){
+            var data = await CallApi({
+                url : `http://localhost:8080/api/detailproducts/${id}`,
+                method : 'get'
+            })
+            setProduct(data);
+        }   
+        getProduct();
+    }, [])
+    function onShowMoreContent(){
+        setMoreContent(true);
+    }
+    function renderNotify(notify){
+
+    }
+    return (Product &&
+        <div className="DetailProduct">
+            <div className="container">
+                <Breadcum ListBreadcum={[Product.category]} final={Product.title}/>
+                <div className="row">
+                    <DetailProductComponent Product = {Product} Notify={renderNotify}/>
+                    <div class="col-lg-3 css-80">
+                        <div class="css-100">
+                            <div class="css-81 css-1002">
+                                <i class="fas fa-truck .css-82"></i>
+                                <span class="css-83">Sản phẩm được miễn phí giao hàng </span>
+                            </div>
+                            <div class="css-84">
+                                <h1 class="css-85">Chính sách bán hàng</h1>
+                                <ul class="css-86">
+                                    <li class="css-87">
+                                        <i class="fas fa-check css-88"></i>
+                                    Cam kết hàng chính hãng 100%
+                                </li>
+                                    <li class="css-87">
+                                        <i class="fas fa-truck css-88"></i>
+                                    Miễn phí giao hàng từ 500K
+                                </li>
+                                    <li class="css-87">
+                                        <i class="fas fa-undo-alt css-88"></i>
+                                    Đổi trả miễn phí trong 10 ngày
+                                </li>
+                                </ul>
+                            </div>
+                            <div class="css-84">
+                                <h1 class="css-85">Dịch vụ khác</h1>
+                                <ul class="css-86">
+                                    <li class="css-87">
+                                        <i class="fas fa-cog css-88"></i>
+                                    Sửa chữa đồng giá 150.000đ.
+                                </li>
+                                    <li class="css-87">
+                                        <i class="fas fa-laptop css-88"></i>
+                                    Vệ sinh máy tính, laptop.
+                                </li>
+                                    <li class="css-87">
+                                        <i class="fas fa-shield-alt css-88"></i>
+                                    Bảo hành tại nhà.
+                                </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="home col-lg-12">
+                        <div class="header-part-2 ">
+                            <div class="header-text">
+                                <h1 class="header-title-1">Sản phẩm tương tự</h1>
+                                <a href="../Trangsanpham/banchay.html" class="header-a">Xem tất cả</a>
+                            </div>
+                        </div>
+                        <SlideProduct/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="home col-lg-12">
+                        <div class="header-part-2 ">
+                            <div class="header-text">
+                                <h1 class="header-title-1">Sản phẩm đi kèm</h1>
+                                <a href="../Trangsanpham/banchay.html" class="header-a">Xem tất cả</a>
+                            </div>
+                        </div>
+                        <SlideProduct/>
+                    </div>
+                </div>
+           
+                <div className="home2">
+                    <div className="product-info " style={MoreContent ? {height:"inherit"} : {}}>
+                    <div className="product-description col-lg-8 pr-5">
+                            <h1 class="header-h1-1">Mô tả sản phẩm</h1>
+                            <div className="product-description-wrap">
+                                {parse(Product.content)}
+                            </div>
+
+                    </div>
+                        <ConfigProduct/>
+                        {!MoreContent && <div class="product-btn-1">
+                         <span onClick={onShowMoreContent}>Xem thêm nội dung</span> <i class="fas fa-chevron-down"></i>
+                      </div>}
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-12">
+                        <Comment id={id}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DetailProduct
